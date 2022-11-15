2409ddb2968bab1c68fe7c82876209f1d27e8ddf
Dmitriy_Malakhov <lesnichiy1986@gmail.com>
2022-11-15 15:41:54 +0300

    изменения в task01

--- Task01/Program.cs
+++ Task01/Program.cs
@@ -15,8 +15,8 @@ Console.WriteLine("Введите второе число: ");
 int number2 =  Convert.ToInt32(Console.ReadLine());
 
 if (number1 == number2 * number2) {
-    Console.WriteLine("Да");
+    Console.WriteLine($"Да, число {number1} является квадратом числа {number2}");
 }
 else {
-     Console.WriteLine("Нет");
+     Console.WriteLine($"Нет, число {number1} не является квадратом числа {number2}");
 }
