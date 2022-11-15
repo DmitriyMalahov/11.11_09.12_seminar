f6e999ad8581795907d870a07d7fe681933053c3
Dmitriy_Malakhov <lesnichiy1986@gmail.com>
2022-11-15 17:06:45 +0300

    add Тask03 changes have been made

--- Task00/Program.cs
+++ Task00/Program.cs
@@ -9,7 +9,7 @@
 
 Console.WriteLine("Введите целое число: ");
 
-int number =  Convert.ToInt32(Console.ReadLine());
+int number = Convert.ToInt32(Console.ReadLine());
 int square = number * number;
 
 Console.WriteLine($"Квадрат числа {number} = {square}");
--- Task01/Program.cs
+++ Task01/Program.cs
@@ -8,15 +8,17 @@
 
 Console.WriteLine("Введите первое число: ");
 
-int number1 =  Convert.ToInt32(Console.ReadLine());
+int number1 = Convert.ToInt32(Console.ReadLine());
 
 Console.WriteLine("Введите второе число: ");
 
-int number2 =  Convert.ToInt32(Console.ReadLine());
+int number2 = Convert.ToInt32(Console.ReadLine());
 
-if (number1 == number2 * number2) {
+if (number1 == number2 * number2)
+{
     Console.WriteLine($"Да, число {number1} является квадратом числа {number2}");
 }
-else {
-     Console.WriteLine($"Нет, число {number1} не является квадратом числа {number2}");
+else
+{
+    Console.WriteLine($"Нет, число {number1} не является квадратом числа {number2}");
 }
