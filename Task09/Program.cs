25c89fca55a5c2bf096e9a4634c1f1c765545703
Dmitriy_Malakhov <lesnichiy1986@gmail.com>
2022-11-18 19:29:43 +0300

    Аdd change Task09

--- Task09/Program.cs
+++ Task09/Program.cs
@@ -9,8 +9,25 @@
 int number = new Random().Next(10, 100);
 Console.WriteLine($"Случайное число из отрезка 10 - 99 --> {number}"); // 78
 
-int firstDigit = number / 10; // 7
-int secondDigit = number % 10; // 8
+// int firstDigit = number / 10; // 7
+// int secondDigit = number % 10; // 8
 
-if (firstDigit > secondDigit) Console.WriteLine($"Наибольшая цифра числа --> {firstDigit}");
-else Console.WriteLine($"Наибольшая цифра числа --> {secondDigit}");
+// if (firstDigit > secondDigit) Console.WriteLine($"Наибольшая цифра числа --> {firstDigit}");
+// else Console.WriteLine($"Наибольшая цифра числа --> {secondDigit}");
+
+// System.Console.WriteLine(Math.Max(firstDigit,secondDigit)); (то же самое, находит наибольшее из двух чисел).
+
+int MaxDigit(int num) // int num = number
+{
+    int firstDigit = num / 10; // 7
+    int secondDigit = num % 10; // 8
+    // if (firstDigit > secondDigit) return firstDigit;
+    // return secondDigit;
+
+    // int max = firstDigit > secondDigit ? firstDigit : secondDigit; // "?" - если(if); ":" - тогда(else). Тернарный оператор.
+    // return max;
+    return firstDigit > secondDigit ? firstDigit : secondDigit;
+}
+
+int maxDigit = MaxDigit(number);
+Console.WriteLine($"Наибольшая цифра числа --> {maxDigit}");
