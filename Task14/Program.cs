6c61f975dc282d13c0f5a3b6b88563d10262b6b9
Dmitriy_Malakhov <lesnichiy1986@gmail.com>
2022-11-18 23:50:07 +0300

    Аdd Task14 and Тask16

---
+++ Task14/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача № 14.
+// Напишите программу, котрая
+// 1. принимает на вход число и
+// 2. проверяет кратно ли оно одновременно 7 и 23.
+
+// 14 -> нет
+// 46 -> нет
+// 161 -> да
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+// bool Multiplicity(int num)
+// {
+//     if (num % 7 == 0 && num % 23 == 0) return true;
+//     return false;
+// }
+
+bool Multiplicity(int num)
+{
+return (num % 7 == 0) && (num % 23 == 0);
+}
+
+bool result = Multiplicity(number);
+
+Console.WriteLine(result ? "да" : "нет");
+
---
+++ Task16/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача № 16.
+// Напишите программу, котрая
+// 1. принимает ДВА числа и
+// 2. проверяет, является ли одно число квадратом другого.
+
+// 5, 25 -> да
+// -4, 16 -> да
+// 25, 5 -> да
+// 8, 9 -> нет
+
+Console.WriteLine("Введите первое число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+bool Square(int num1, int num2)
+{
+return (num1 == num2 * num2) || (num2 == num1 * num1);
+}
+
+bool result = Square(number1, number2);
+
+Console.WriteLine(result ? "да" : "нет");
