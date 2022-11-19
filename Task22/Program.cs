c86cfced838e439dcf808c3ae70c316b1b0d898b
Dmitriy_Malakhov <lesnichiy1986@gmail.com>
2022-11-19 19:39:10 +0300

    Add Task22

---
+++ Task22/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача № 22:
+// Напишите программу, которая
+// 1. принимает на вход число (N) и 
+// 2. выдает таблицу квадратов от 1 до N.
+
+// 5 -> 1,4,9,16,25
+// 2 -> 1,4
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Таблица квадратов от 1 до {number}: ");
+void TableSquare(int num)
+{
+    for (int count = 1; count <= num; count++)
+    {
+                Console.WriteLine($" {count}   |  {count*count} ");
+    }
+    return;
+}
+
+TableSquare(number);
+
+
