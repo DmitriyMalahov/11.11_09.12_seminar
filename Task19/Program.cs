2969eca98767c5bd9051328a92d18843c30923c0
Dmitriy_Malakhov <lesnichiy1986@gmail.com>
2022-11-20 00:53:36 +0300

    Task19  edits

--- Task19/Program.cs
+++ Task19/Program.cs
@@ -10,21 +10,52 @@
 Console.WriteLine("Введите пятизначное число: ");
 int number = Convert.ToInt32(Console.ReadLine());
 
-while (number < 10000 & number <= 100000)
+if (number < 0)
 {
-    Console.WriteLine("Введите пятизначное число: ");
-    number = Convert.ToInt32(Console.ReadLine());
-    if (number < 10000 & number <= 100000) Console.WriteLine("Введено некорректное значение"); 
+    number = number * -1;
 }
 
-int number1 = number;
-int numberInverted = 0;
-
-while (number1 > 0)
+bool Check(int numCheck)
 {
-    int count = number1 % 10;
-    numberInverted = numberInverted * 10 + count;
-    number1 = number1 / 10;
+    return (number >= 10000) && (number < 100000);
+}
+bool check = Check(number);
+Console.WriteLine(check ? $"Является ли введенное число палиндромом? Ответ:" : "Введено не пятизначное число");
+
+int Invert(int num)
+{
+    int number1 = num;
+    int numberInv = 0;
+
+    while (number1 > 0)
+    {
+        int count = number1 % 10;
+        numberInv = numberInv * 10 + count;
+        number1 = number1 / 10;
+    }
+    return numberInv;
 }
 
-Console.WriteLine(number == numberInverted ? "да" : "нет");
+int Inverted = Invert(number);
+
+Console.WriteLine(number == Inverted && check ? "да" : "нет");
+
+
+// while (number < 10000 && number <= 100000)
+// {
+//     Console.WriteLine("Введите пятизначное число: ");
+//     number = Convert.ToInt32(Console.ReadLine());
+//     if (number < 10000 && number <= 100000) Console.WriteLine("Введено некорректное значение"); 
+// }
+
+// int number1 = number;
+// int numberInverted = 0;
+
+// while (number1 > 0)
+// {
+//     int count = number1 % 10;
+//     numberInverted = numberInverted * 10 + count;
+//     number1 = number1 / 10;
+// }
+
+// Console.WriteLine(number == numberInverted ? "да" : "нет");
