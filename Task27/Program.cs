f35c1745fc64c950a4880b7a17dd913805bbaba3
Dmitriy_Malakhov <lesnichiy1986@gmail.com>
2022-11-22 17:18:00 +0300

    Add Task27 from DZ Seminar 4

--- Task27/Program.cs
+++ Task27/Program.cs
@@ -7,3 +7,35 @@
 // 82 -> 10
 // 9012 -> 12
 
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number < 0)
+{
+    number = Math.Abs(number);
+}
+
+bool Check(int num)
+{
+    return (num > 0);
+}
+
+int SumDigits(int number1)
+{
+    number1 = number;
+    int numberInv = 0;
+    int sum = 0;
+
+    while (number1 > 0)
+    {
+        int digit = number1 % 10;
+        numberInv = numberInv * 10 + digit;
+        number1 = number1 / 10;
+        sum = digit + sum;
+    }
+    return sum;
+}
+
+int sumDigits = SumDigits(number);
+bool check = Check(number);
+Console.WriteLine(check ? $"Cумма цифр числа {number} равна = {sumDigits}:" : "Введено не корректное значение");
