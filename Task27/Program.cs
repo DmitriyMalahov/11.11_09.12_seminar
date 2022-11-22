dad423f805499612200e14773758ea545ccf7a54
Dmitriy_Malakhov <lesnichiy1986@gmail.com>
2022-11-22 20:46:02 +0300

    Add Task29 from DZ Seminar 4

--- Task27/Program.cs
+++ Task27/Program.cs
@@ -10,32 +10,36 @@
 Console.WriteLine("Введите число: ");
 int number = Convert.ToInt32(Console.ReadLine());
 
+
+
 if (number < 0)
 {
     number = Math.Abs(number);
 }
 
+
 bool Check(int num)
 {
-    return (num > 0);
+    return (num > 0); 
 }
 
 int SumDigits(int number1)
 {
     number1 = number;
-    int numberInv = 0;
+    int numberInverted = 0;
     int sum = 0;
 
     while (number1 > 0)
     {
-        int digit = number1 % 10;
-        numberInv = numberInv * 10 + digit;
+        int remains = number1 % 10;
+        numberInverted = numberInverted * 10 + remains;
         number1 = number1 / 10;
-        sum = digit + sum;
+        sum = remains + sum;
     }
+
     return sum;
 }
 
 int sumDigits = SumDigits(number);
 bool check = Check(number);
-Console.WriteLine(check ? $"Cумма цифр числа {number} равна = {sumDigits}:" : "Введено не корректное значение");
+Console.WriteLine(check ?  $"Cумма цифр числа {number} равна = {sumDigits}" : "Введено не корректное значение");
--- Task29/Program.cs
+++ Task29/Program.cs
@@ -5,3 +5,24 @@
 
 // 1, 2, 5, 7, 19, 6, 1, 33 -> [1, 2, 5, 7, 19, 6, 1, 33]
 
+int[] array = new int[8];
+
+void RandomArray(int[] arrayLocal)
+{
+    Random rnd = new Random();
+    for (int i = 0; i < array.Length; i++)
+    {
+        arrayLocal[i] = rnd.Next(0, 33);
+    }
+}
+
+void PrintArray(int[] arrayLocal)
+{
+    for (int i = array.Length - 1; i >= 0; i--)
+    {
+        Console.Write($"{arrayLocal[i]}, ");
+    }
+}
+
+RandomArray(array);
+PrintArray(array);
