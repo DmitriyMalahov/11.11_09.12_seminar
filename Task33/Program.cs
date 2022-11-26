9f04952dd46f332ef09cb0278f8da35ef8ddf604
Dmitriy_Malakhov <lesnichiy1986@gmail.com>
2022-11-26 17:19:15 +0300

    Add Task33

--- Task33/Program.cs
+++ Task33/Program.cs
@@ -1,9 +1,10 @@
-﻿// Задача 32:
-// Напишите программу которая будет
-// 1. заменять элементы массива:
-// положительные элементы замените на соответствующие отрицательные, и наоборот.
+﻿// Задача 33:
+// 1. Задайте массив.
+// Напишите программу, которая
+// 2. определяет, присуствует ли заданное число в массиве.
 
-// [-4, -8, 8, 2] -> [4, 8, -8, -2]
+// 4; массив [6,7,19,345,3] -> нет 
+// 3; массив [6,7,19,345,3] -> да
 
 Console.WriteLine("Введите длину массива: ");
 int sizeArray = Convert.ToInt32(Console.ReadLine());
@@ -11,8 +12,10 @@ Console.WriteLine("Введите минимальное значение мас
 int minArray = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите максимальное значение массива: ");
 int maxArray = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число которое необходимо найти в массиве: ");
+int number = Convert.ToInt32(Console.ReadLine());
 
-int[] CreateArrayRndInt (int size, int min, int max)
+int[] CreateArrayRndInt(int size, int min, int max)
 {
     int[] arr = new int[size];
     Random rnd = new Random();
@@ -20,30 +23,31 @@ int[] CreateArrayRndInt (int size, int min, int max)
     for (int i = 0; i < arr.Length; i++)
     {
         arr[i] = rnd.Next(min, max + 1);
+
     }
     return arr;
-} 
+}
 
-void PrintArray (int[] arr)
+void PrintArray(int[] arr)
 {
     Console.Write("[");
     for (int i = 0; i < arr.Length; i++)
     {
-        if(i < arr.Length - 1) Console.Write($"{arr[i]},");
+        if (i < arr.Length - 1) Console.Write($"{arr[i]},");
         else Console.Write($"{arr[i]}");
     }
     Console.WriteLine("]");
 }
 
-void NegativeArray (int[] arr)
+bool PresenceNumber(int[] arr, int num)
 {
     for (int i = 0; i < arr.Length; i++)
     {
-        arr[i] *= -1; // arr[i] = arr[i] * -1;
+        if (arr[i] == num) return true;
     }
+    return false;
 }
 
 int[] array = CreateArrayRndInt(sizeArray, minArray, maxArray);
 PrintArray(array);
-NegativeArray(array);
-PrintArray(array);
+Console.WriteLine(PresenceNumber (array, number) ? "Да" : "Нет");
