b0dffa2406aa939742b7c0fe535e8ed2c5de548c
Dmitriy_Malakhov <lesnichiy1986@gmail.com>
2022-12-05 23:44:40 +0300

    Add Tasks 49

---
+++ Task49/Program.cs
@@ -0,0 +1,66 @@
+﻿// Задача 49:
+// 1. Задайте двумерный массив.
+// 2. Найдите элементы, у которых оба индекса четные,
+// и замените эти элементы на  их квадраты.
+
+// 1 4 7 2    1  4 49 2
+// 5 9 2 3    5  9 2  3
+// 8 4 2 4    64 4 4  4
+
+
+
+
+int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns];
+    Random rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("|");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j],5}|");
+            else Console.Write($"{matrix[i, j],5}");
+        }
+        Console.WriteLine("|");
+    }
+}
+
+void SquareEvenElMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (i % 2 == 0 && j % 2 == 0) matrix[i, j] *= matrix[i, j];
+        }
+    }
+}
+
+Console.WriteLine("Введите количество строк: ");
+int rowsMatrix = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите количество столбцов: ");
+int columnsMatrix = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите минимальное значение массива: ");
+int minMatrix = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите максимальное значение массива: ");
+int maxMatrix = Convert.ToInt32(Console.ReadLine());
+
+int[,] array2D = CreateMatrixRndInt(rowsMatrix, columnsMatrix, minMatrix, maxMatrix);
+PrintMatrix(array2D);
+Console.WriteLine();
+SquareEvenElMatrix(array2D);
+PrintMatrix(array2D);
