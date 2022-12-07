41854a3cd40330185f07d6684c111fd004de1d06
Dmitriy_Malakhov <lesnichiy1986@gmail.com>
2022-12-07 12:32:39 +0300

    Add Task53

---
+++ Task53/Program.cs
@@ -0,0 +1,61 @@
+﻿// Задача 53:
+// 1. Задайте двумерный массив.
+// 2. Напишите программу, которая поменяет местами
+// первую и последнюю строку массива.
+
+int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns];
+    Random rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix, string symbol1, string symbol2, string symbol3)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write($"{symbol1}");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j],5}{symbol2}");
+            else Console.Write($"{matrix[i, j],5}");
+        }
+        Console.WriteLine($"{symbol3}");
+    }
+}
+
+
+void ChangeRows(int[,] matrix)
+{
+    int temp = 0;
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        temp = matrix[0, j];
+        matrix[0, j] = matrix[matrix.GetLength(0)-1, j];
+        matrix[matrix.GetLength(0)-1, j] = temp;
+    }
+}
+
+
+Console.WriteLine("Введите количество строк: ");
+int rowsMatrix = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите количество столбцов: ");
+int columnsMatrix = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите минимальное значение массива: ");
+int minMatrix = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите максимальное значение массива: ");
+int maxMatrix = Convert.ToInt32(Console.ReadLine());
+
+int[,] array2D = CreateMatrixRndInt(rowsMatrix, columnsMatrix, minMatrix, maxMatrix);
+PrintMatrix(array2D, "|", "|", "|"); ;
+Console.WriteLine();
+ChangeRows(array2D);
+PrintMatrix(array2D, "|", "|", "|"); ;
