e5fe7bf6e2dd69eaa0ec94d32d9c4050f0c9b243
Dmitriy_Malakhov <lesnichiy1986@gmail.com>
2022-12-10 23:08:56 +0300

    Add Task60

--- Task60/Program.cs
+++ Task60/Program.cs
@@ -8,3 +8,42 @@
 
 // 66(0,0,0) 27(0,0,1) 25(0,1,0) 90(0,1,1)
 // 34(1,0,0) 26(1,0,1) 41(1,1,0) 55(1,1,1)
+
+int[,,] CreateMatrixRndInt(int rows, int columns, int depth, int min, int max)
+{
+    int[,,] matrix = new int[rows, columns, depth];
+    Random rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            for (int k = 0; k < matrix.GetLength(2); k++)
+            {
+                matrix[i, j, k] = rnd.Next(min, max);
+            }
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            for (int k = 0; k < matrix.GetLength(2); k++)
+            {
+                if (k < matrix.GetLength(2) - 1) Console.Write($"{matrix[i, j, k],5}({i},{j},{k})");
+                else Console.Write($"{matrix[i, j, k],5}({i},{j},{k})");
+            }
+            Console.WriteLine();
+        }
+    }
+}
+
+int[,,] matrix1 = CreateMatrixRndInt(2, 2, 2, 10, 99);
+Console.WriteLine("Исходная матрица:");
+PrintMatrix(matrix1); ;
+Console.WriteLine();
