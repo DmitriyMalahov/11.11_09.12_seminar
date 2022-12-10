8b0e001e0d0e131d926cf049294c797784a13d43
Dmitriy_Malakhov <lesnichiy1986@gmail.com>
2022-12-10 17:07:45 +0300

    Add Task54

--- Task54/Program.cs
+++ Task54/Program.cs
@@ -12,3 +12,62 @@
 // 9 5 3 2
 // 8 4 4 2
 
+int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns];
+    Random rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix, string symbol1, string symbol2, string symbol3)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write($"{symbol1}");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j],5}{symbol2}");
+            else Console.Write($"{matrix[i, j],5}");
+        }
+        Console.WriteLine($"{symbol3}");
+    }
+}
+
+void DescendingOrderingElementsColums(int[,] matrix)
+{
+    int rows = matrix.GetLength(0);
+    int columns = matrix.GetLength(1);
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            for (int k = 0; k < columns - 1; k++)
+            {
+                if (matrix[i, k] > matrix[i, k + 1])
+                {
+                    int temp = 0;
+                    temp = matrix[i, k];
+                    matrix[i, k] = matrix[i, k + 1];
+                    matrix[i, k + 1] = temp;
+                }
+            }
+        }
+    }
+}
+
+int[,] array2D = CreateMatrixRndInt(4, 10, 1, 9);
+Console.WriteLine("Исходный массив:");
+PrintMatrix(array2D, "|", "|", "|"); ;
+Console.WriteLine();
+Console.WriteLine("Упорядочинный массив по убыванию элементы каждой строки:");
+DescendingOrderingElementsColums(array2D);
+PrintMatrix(array2D, "|", "|", "|"); ;
+
