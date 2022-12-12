253c02941f58fc938326641e995cd699a944c399
Dmitriy_Malakhov <lesnichiy1986@gmail.com>
2022-12-12 22:36:13 +0300

    Add Task67 and Task69

---
+++ Task69/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 69:
+
+// Напишите программу, которая на вход принимает два  числа А и В, и
+// возводит число А в целую степень В с помощью рекурсии.
+
+// А = 3; В = 5 -> 243 
+// А = 2; В = 3 -> 8
+
+Console.Write("Введите первое натуральное число:");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе натуральное число:");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+int Exponentiation (int num, int degree)
+{
+    if (degree > 1)
+    return num * Exponentiation(num, degree-1);
+    return num;
+}
+
+Console.WriteLine(Exponentiation(number1, number2));
