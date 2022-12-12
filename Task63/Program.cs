5f91fb6851d4f3afd79c3bf159a427e0263f60e6
Dmitriy_Malakhov <lesnichiy1986@gmail.com>
2022-12-12 20:58:23 +0300

    Add Task63 and Task65

---
+++ Task63/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 63:
+// 1. Задайте значение N. 
+// 2. Напишите программу, которая выдает все 
+// натруральные числа в промежутке от 1 до N.
+
+// N = 5 -> "1, 2, 3, 4, 5"
+// N = 6 -> "1, 2, 3, 4, 5, 6"
+
+
+Console.WriteLine("Введите натуральное число:");
+int number = Convert.ToInt32(Console.ReadLine());
+
+NaturalNumbers(number);
+
+void NaturalNumbers(int num)
+{
+    if (num == 0) return;
+    NaturalNumbers(num - 1);
+    Console.Write($"{num} ");
+}
---
+++ Task65/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 65:
+// 1. Задайте значения M и N.
+// 2. Напишите программу, которая
+// выдает все натуральные числа в промежутке от M до N.
+
+// M = 1; N = 5 -> "1, 2, 3, 4, 5"
+// M = 4; N = 8 -> "4, 5, 6, 7, 8"
+
+Console.WriteLine("Введите натуральное число 1:");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите натуральное число 2:");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+NaturalNumbers(number1, number2);
+
+void NaturalNumbers(int num1, int num2)
+{
+    if (num1 > num2)
+    {
+        NaturalNumbers(num1, num2 + 1);
+        Console.Write($"{num2} ");
+    }
+    if (num1 < num2)
+    {
+        Console.Write($"{num1} ");
+        NaturalNumbers(num1 + 1, num2);
+    }
+    if (num1 == num2)
+        Console.Write($"{num1} ");
+}
